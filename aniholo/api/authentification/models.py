b26f002ec8632b7aa0b002733f8aaa8a405ab389
JustRandomUsr <justrandomusr@gmail.com>
2019-02-11 22:57:08 +0100

    change email data type

--- aniholo/api/authentification/models.py
+++ aniholo/api/authentification/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
 	user_id = models.TextField(primary_key=True, max_length=50)
 	username = models.CharField(unique=True, max_length=50)
 	password = models.CharField(max_length=100, null=False)
-	email = models.CharField(null=False)
+	email = models.TextField(null=False)
 	date_joined = models.IntegerField()
 	last_login = models.IntegerField()
 
