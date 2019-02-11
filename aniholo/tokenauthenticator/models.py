11f9f835be878d36e543f7818b71ed58135a1cb9
Imran Khaliq <ikhaliq15@yahoo.com>
2019-02-11 00:43:32 -0800

    upload the days work.
    models of db objects, some command api's written. what a day. and made some new friends. ;)

--- aniholo/tokenauthenticator/models.py
+++ aniholo/tokenauthenticator/models.py
@@ -15,16 +15,55 @@ class Account(models.Model):
 		managed = False
 		db_table = 'account'
 
+# class Posts(models.Model):
+#     post_id = models.AutoField(primary_key=True)
+#     author = models.CharField(max_length=150)
+#     title = models.CharField(max_length=1000)
+#     upvotes = models.CharField(max_length=1000)
+#     downvotes = models.CharField(max_length=1000)
+#     date_posted = models.DateTimeField()
+#     image_link = models.CharField(max_length=250, blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'posts'
+#         app_label = 'post_stuff'
+
 class Posts(models.Model):
     post_id = models.AutoField(primary_key=True)
-    author = models.CharField(max_length=150)
-    title = models.CharField(max_length=1000)
-    upvotes = models.CharField(max_length=1000)
-    downvotes = models.CharField(max_length=1000)
-    date_posted = models.DateTimeField()
+    author = models.CharField(max_length=150, blank=True, null=True)
+    title = models.CharField(max_length=1000, blank=True, null=True)
+    upvotes = models.CharField(max_length=1000, blank=True, null=True)
+    downvotes = models.CharField(max_length=1000, blank=True, null=True)
+    date_posted = models.DateTimeField(blank=True, null=True)
     image_link = models.CharField(max_length=250, blank=True, null=True)
+    is_nsfw = models.BooleanField(blank=True, null=True)
+    is_spoiler = models.BooleanField(blank=True, null=True)
+    source_material = models.CharField(max_length=1000, blank=True, null=True)
+    required_tag = models.CharField(max_length=30)
+    other_tags = models.CharField(max_length=10000, blank=True, null=True)
+    is_announcement = models.BooleanField(blank=True, null=True)
+    post_text = models.CharField(max_length=10000, blank=True, null=True)
+    post_type = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'posts'
         app_label = 'post_stuff'
+
+class Tags(models.Model):
+    tag_name = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tags'
+        app_label = 'tag_stuff'
+
+class PostTagPivot(models.Model):
+    post_id = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    tag_id = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'post_tag_pivot'
+        app_label = 'post_and_tag_stuff'
