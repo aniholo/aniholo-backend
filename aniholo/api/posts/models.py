893db055792e63a2132c5642a077b6af0a601526
EricTGB <cameronbruce3@gmail.com>
2019-02-13 10:14:59 +1300

    adding comment model

--- aniholo/api/posts/models.py
+++ aniholo/api/posts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
 
 # Create your models here.
 
@@ -33,3 +35,16 @@ class PostTagPivot(models.Model):
         managed = False
         db_table = 'post_tag_pivot'
         app_label = 'post_tag_pivot_connector'
+
+class Comment_temp(MPTTModel):
+    author = models.CharField(null=False, max_length=50)
+    post_id = models.IntegerField(null=False)
+    date_posted = models.IntegerField(null=False)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    score = models.IntegerField(default=0)
+    raw_content = models.TextField(null=False)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children', db_index=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['-score']
