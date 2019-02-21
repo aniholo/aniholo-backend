ca9c69f4f2d3a613950d9be039044577ab3ad9da
Vrx26 <dimon552505@yandex.ru>
2019-02-21 10:31:50 +0300

    tree building functions moved to distinct file

--- aniholo/api/comments/views.py
+++ aniholo/api/comments/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 
 from . import models
+from .utils import get_children, add_nodes
 from aniholo import settings
 from api.authentication import token
 from api.authentication.models import User
@@ -152,45 +153,3 @@ def get_comment(request):
                 'children': []}
     add_nodes(get_children(comment, order_type), tree, user_id, 1, depth, order_type)
     return Response({'status': 'success', 'tree': tree})
-
-
-def add_nodes(q_set, tree, user_id, current_depth, depth, order_type):
-    ''' recursive tree building '''
-    if q_set is None or current_depth == depth:
-        return None
-    i = 0
-    for node in q_set:
-        vote = Vote.objects.filter(user_id=user_id, vote_type=0, object_id=node.comment_id).first()
-        if vote is None:
-            vote = 0
-        else:
-            vote = vote.vote_value
-        if node.status == 2:
-            tree['children'].append({'comment_id': node.comment_id,
-                                     'author_id': "[deleted]",
-                                     'author_name': "[deleted]",
-                                     'date_posted': "[deleted]",
-                                     'score': "[deleted]",
-                                     'user_vote': "[deleted]",
-                                     'content': "[deleted]",
-                                     'children': []})
-        else:
-            tree['children'].append({'comment_id': node.comment_id,
-                                     'author_id': node.author.user_id,
-                                     'author_name': node.author.username,
-                                     'date_posted': node.date_posted,
-                                     'score': node.score,
-                                     'user_vote': vote,
-                                     'content': node.raw_content,
-                                     'parent_id': node.parent_id,
-                                     'children': []})
-        add_nodes(get_children(node, order_type), tree['children'][i], user_id, current_depth + 1, depth, order_type)
-        i += 1
-
-
-def get_children(node, order_type):
-    comments = models.Comment.objects.filter(parent_id=node.comment_id)
-    if order_type == 'best':
-        return comments.order_by('-score')
-    else:
-        return comments.order_by('-date_posted')
